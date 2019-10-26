d383289962fa178100f2507e0db2553bbe20781a
YEIMMY MARITZA GARCIA HERRAN <jemar988@hotmail.com>
2019-10-25 21:09:25 -0500

    se creo tabla servicios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_230645) do
+ActiveRecord::Schema.define(version: 2019_10_26_015947) do
+
+  create_table "servicios", force: :cascade do |t|
+    t.string "nombre"
+    t.text "descripcion"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Servicio.create(nombre: 'Plomero', descripcion: 'Técnicos en Instalación y Mantenimiento de Redes Hidráulicas y Sanitarias ')
+Servicio.create(nombre: 'Gas', descripcion: 'Servicio tecnico especializado en gasodomesticos')
+Servicio.create(nombre: 'Limpieza', descripcion: 'Servicios especializados en limpieza')
+Servicio.create(nombre: 'Electricista', descripcion: 'Técnicos especializados en redes e instalaciones electricas')
+Servicio.create(nombre: 'Carpintero', descripcion: 'Carpinteros especializados en fabricación y reparación de todo tipo de muebles')
+Servicio.create(nombre: 'Cerrajero', descripcion: 'Profesionales capacitados en seguridad manual y sistemas electronicos para hogares y empresas')
+Servicio.create(nombre: 'Pintor', descripcion: 'profesionales en pintura, remodelaciones y acabados')
